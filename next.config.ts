562e4e9136db323c377ec8b2882f1bf8028bea18
Parvez <parvezz13913@gmail.com>
2025-11-06 17:29:29 +0600

    Update the code

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
@@ -16,28 +15,6 @@ const nextConfig = {
         hostname: "192.168.4.13",
         port: "8080",
         pathname: "/public/storage/**",
-=======
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '4010',
-        pathname: '/public/storage/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '192.168.7.14',
-        port: '4010',
-        pathname: '/public/storage/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**', 
->>>>>>> 0d68dfcfa314d961c90c86e8fca59766fcec8abb
       },
     ],
   },
