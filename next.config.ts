bc18e510cf6bed851bbc600017fca66f146810fc
abedinalways <nion.sk5067@gmail.com>
2025-11-06 17:24:29 +0600

    Conflict solved

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "192.168.4.13",
-        port: "8080",
-        pathname: "/public/storage/**",
-=======
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -36,8 +17,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '**', 
->>>>>>> 0d68dfcfa314d961c90c86e8fca59766fcec8abb
+        hostname: '**',
       },
     ],
   },
