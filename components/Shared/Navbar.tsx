6457be6a9b08c3b9c48f012412c67fe6daa73db7
abedinalways <nion.sk5067@gmail.com>
2025-11-09 17:06:07 +0600

    navbar section updated

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { HiMenuAlt3 } from 'react-icons/hi';
+import { useGetMeQuery } from '@/rtk/features/all-apis/auth/authApi';
 
 
 export interface NavbarProps {
@@ -11,7 +12,23 @@ export interface NavbarProps {
   notificationCount: number;
 }
 
+
+
 const Navbar: React.FC<NavbarProps> = ({ onMobileMenuToggle }) => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const { data } = useGetMeQuery("");
+    const [image, setImage] = useState<string | null>(null);
+
+  
+
+  useEffect(() => {
+    if (data?.data) {
+      setFullName(data.data.name || '');
+      setEmail(data.data.email || '');
+      setImage(data.data.avatar_url || null);
+    }
+  }, [data]);
   return (
     <header className="bg-white z-10 border-b border-gray-200 font-[inter]">
       <div className="flex items-center justify-between py-4 px-2 md:px-5 gap-8">
@@ -26,14 +43,14 @@ const Navbar: React.FC<NavbarProps> = ({ onMobileMenuToggle }) => {
 
           {/* greetings */}
           <div className="relative flex-1 text-xs md:text-lg">
-            <h2>Hello Sheikh</h2>
+            <h2>Hello {fullName}</h2>
             <p>Good Morning!</p>
           </div>
         </div>
         {/* Right side - User dropdown */}
         <div className="flex items-center gap-4 text-xs md:text-lg">
           <Image
-            src="/images/profile.png"
+            src={image ?? '/default-avatar.png'}
             width={40}
             height={40}
             className="rounded-md"
@@ -41,7 +58,7 @@ const Navbar: React.FC<NavbarProps> = ({ onMobileMenuToggle }) => {
           />
           <div>
             <h2>Sheikh</h2>
-            <p>Web Developer</p>
+            <p>{email}</p>
           </div>
         </div>
       </div>
