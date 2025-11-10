0960fd1639e1502a01f5be070fdc655130a9f6ef
Parvez <parvezz13913@gmail.com>
2025-11-10 15:10:39 +0600

    Solved all the issue

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '192.168.7.14',
-        port: '4040',
-        pathname: '/public/storage/**',
+        protocol: "http",
+        hostname: "192.168.7.14",
+        port: "4040",
+        pathname: "/public/storage/**",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.4.13",
+        port: "8080",
+        pathname: "/public/storage/**",
       },
     ],
   },
