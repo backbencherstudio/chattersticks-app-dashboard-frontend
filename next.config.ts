aef18376f48b2b515e86b0d9a2a1361e3381c05c
parvez13913 <parvezz13913@gmail.com>
2025-11-29 12:31:01 +0600

    Image issue fix

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,22 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '192.168.7.14',
-        port: '4010', 
-        pathname: '/public/storage/**',
+        protocol: "http",
+        hostname: "192.168.7.14",
+        port: "4010",
+        pathname: "/public/storage/**",
       },
       {
-        protocol: 'http',
-        hostname: '192.168.4.13',
-        port: '8080',
-        pathname: '/public/storage/**',
+        protocol: "http",
+        hostname: "192.168.4.13",
+        port: "8080",
+        pathname: "/public/storage/**",
+      },
+      {
+        protocol: "http",
+        hostname: "31.97.214.154",
+        port: "4000",
+        pathname: "/public/storage/**",
       },
     ],
   },
